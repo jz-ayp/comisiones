92ec5567abfa9b38234fb54a0fadcd9df825ea59
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-27 15:03:43 -0600

    Update main.py
    Code solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
-"""
+
 
 # Declaraciones
 CONSTANTE = valor
@@ -13,3 +13,23 @@ entrada = input()
 
 # Salidas
 print(salida)
+"""
+
+
+# Entradas
+vtas_lun = eval(input("Introduzca las ventas del lunes: "))
+vtas_mar = eval(input("Introduzca las ventas del martes: "))
+vtas_mie = eval(input("Introduzca las ventas del miércoles: "))
+vtas_jue = eval(input("Introduzca las ventas del jueves: "))
+vtas_vie = eval(input("Introduzca las ventas del viernes: "))
+
+# Proceso
+total = vtas_lun + vtas_mar + vtas_mie + vtas_jue + vtas_vie;
+if total <= 20000:
+    comision = total * 0.05
+else:
+	comision = total * 0.06
+
+# Salidas
+print("Ventas totales:", total)
+print("Comisión:", comision)
