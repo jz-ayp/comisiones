2a6c90f8742352a3c3eed16ac96d6fa0c4ba6175
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-06 00:56:18 -0600

    Remove solution

--- comisiones.py
+++ comisiones.py
@@ -1,18 +1,15 @@
 """
-Cálculo de total de ventas y comisiones.
+Inserta el encabezado aquí y escribe tu código abajo
 """
 
+# Declaraciones
+CONSTANTE = valor
+
 # Entradas
-lu, ma, mi, ju, vi = [float(input(f"Ventas del {dia}: ")) for dia in ("lunes", "martes", "miércoles", "jueves", "viernes")]
+entrada = input()
 
 # Proceso
-total = sum((lu, ma, mi, ju, vi))
-if total <= 20_000:
-    tasa = 0.05
-else:
-    tasa = 0.06
-comision = tasa * total
+
 
 # Salidas
-print("Total de ventas:", total)
-print("Comisión:", comision)
+print(salida)
